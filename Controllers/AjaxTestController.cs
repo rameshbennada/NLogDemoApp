71f0c62435e311c0a7588040ce45d6b2009920d6
rameshbennada <rammi408@gmail.com>
2025-02-22 17:23:24 +0530

    Add project files.

---
+++ Controllers/AjaxTestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NLogDemoApp.Controllers
+{
+    public class AjaxTestController : Controller
+    {
+        
+        // GET: /AjaxTest/
+        [HttpGet]
+        public ActionResult FirstAjax()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FirstAjax(string a)
+        {
+            return Json("chamara", JsonRequestBehavior.AllowGet);
+        }
+        
+    }
+}
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,60 @@
+﻿using NLogDemoApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NLog;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Entity;
+namespace NLogDemoApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        private static Logger logger = LogManager.GetLogger("myAppLoggerRules");
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View("login");
+        }
+
+        //[HttpPost]
+        //public ActionResult Login(UserModel objUser)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        using (NlogEntities3 db = new NlogEntities3())
+        //        {
+        //            var obj = db.users.Where(a => a.username.Equals(objUser.username) && a.password.Equals(objUser.password)).FirstOrDefault();
+        //            if (obj != null)
+        //            {
+        //                Session["UserID"] = obj.id.ToString();
+        //                Session["UserName"] = obj.username.ToString();
+
+        //                logger.Info("Login  -- Suceess","", "myAppLoggerRules");
+
+        //                return RedirectToAction("UserDashBoard");
+        //            }
+        //        }
+        //    }
+           
+        //    logger.Info("Login -- failure");
+
+        //    return View("errorlogin");
+            
+        //}
+        
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
